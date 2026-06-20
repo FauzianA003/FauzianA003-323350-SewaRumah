890451d858e079da66b89591a6a3165f5350d52b
FauzianA003 <mohafshor@gmail.com>
2026-06-20 16:45:06 +0700

    fix public index.php laravel vercel

--- public/index.php
+++ public/index.php
@@ -5,15 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Check maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Load autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
